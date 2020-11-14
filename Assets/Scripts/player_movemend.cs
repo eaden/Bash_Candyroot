8c7d2013dcc4f12f00b590ed5d68dbff75d063bf
Ingo2828 <dennis.schneeweiss@web.de>
2020-11-14 19:57:14 +0100

    dinge

--- Assets/Scripts/player_movemend.cs
+++ Assets/Scripts/player_movemend.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class player_movemend : MonoBehaviour
 {
-
+        float duckingTimeLimit = 2;
+        float duckingTime = 2;
+        GameObject childcube;
         bool isJumping = false;
+        bool isDucking = false;
         Rigidbody rigi;
         private float posx;
     // Start is called before the first frame update
     void Start()
     {
+        childcube = transform.Find("ChildCube").gameObject;
         rigi = GetComponent<Rigidbody>();
         posx = 1;
+        GetComponent<FixedJoint>().connectedBody = childcube.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         rigi.velocity = new Vector3(rigi.velocity.x, rigi.velocity.y, -2f);
         if(Input.GetKeyDown(KeyCode.RightArrow))
         {
@@ -36,19 +43,40 @@ public class player_movemend : MonoBehaviour
             }
             
         }
-        if(Input.GetKeyDown(KeyCode.UpArrow) && isJumping == false)
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isJumping == false && isDucking == false)
         {
             rigi.AddForce(new Vector3(0f, 8f, 0f),ForceMode.VelocityChange);
             isJumping = true;
         }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && isJumping == false && isDucking == false)
+        {
+            
+
+            childcube.SetActive(false);
+            isDucking = true;
+           
+
         }
+        if(isDucking == true)
+        {
+            duckingTime -= Time.deltaTime;
+        }
+        
+        
+        if (duckingTime < 0)
+        {
+            isDucking = false;
+            duckingTime = duckingTimeLimit;
+            childcube.SetActive(true);
+        }
+
+    }
        void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag == "Floor")
         {
             isJumping = false;
         }
-
 
     }
    
