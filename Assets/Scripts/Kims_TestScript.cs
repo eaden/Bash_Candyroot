125e86d821f0161980e1f956a52a7dae3160212d
K K <kmk@gmx.de>
2020-11-14 22:38:36 +0100

    KimScene

---
+++ Assets/Scripts/Kims_TestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kims_TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("tt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
