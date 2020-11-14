201fc20f22855f892caec1fe5ed0f98bc398bb9a
Janine <janine.bissinger@st.ovgu.de>
2020-11-14 11:11:02 +0100

    Code Bewegung

---
+++ Assets/Scripts/Bewegung.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bewegung : MonoBehaviour
+{
+    float posx = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            
+            if (posx < 2)
+            {
+                posx += 1f;
+                gameObject.transform.position += new Vector3(1f, 0f, 0f);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (posx > 0)
+            {
+                posx -= 1f;
+                gameObject.transform.position += new Vector3(-1f, 0f, 0f);
+            } 
+        }
+    }
+}
