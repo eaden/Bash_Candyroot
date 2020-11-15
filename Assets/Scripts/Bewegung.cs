f6026f4d61569f2057b98920bbf5d1e7f8494061
Janine <janine.bissinger@st.ovgu.de>
2020-11-15 10:41:02 +0100

    ignore

--- Assets/Scripts/Bewegung.cs
+++ Assets/Scripts/Bewegung.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class Bewegung : MonoBehaviour
 {
     float posx = 1f;
+    Rigidbody rigi;
+    bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigi = GetComponent<Rigidbody>();
     }
 
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Floor")
+        {
+            isJumping = false;
+        }
+
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+
+
+    
     // Update is called once per frame
     void Update()
     {
+        rigi.velocity = new Vector3(rigi.velocity.x, rigi.velocity.y, -2f);
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             
             if (posx < 2)
             {
                 posx += 1f;
-                gameObject.transform.position += new Vector3(1f, 0f, 0f);
+                gameObject.transform.position += new Vector3(1.5f, 0f, 0f);
             }
         }
 
@@ -30,8 +53,25 @@ public class Bewegung : MonoBehaviour
             if (posx > 0)
             {
                 posx -= 1f;
-                gameObject.transform.position += new Vector3(-1f, 0f, 0f);
+                gameObject.transform.position += new Vector3(-1.5f, 0f, 0f);
             } 
         }
+
+        //springen
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (posx > 0)
+            {
+                posx -= 1f;
+                gameObject.transform.position += new Vector3(0f, -1f, 0f);
+            }
+        }
+
+        //ducken
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigi.AddForce(new Vector3(0f, 8f, 0f),ForceMode.VelocityChange);
+        }
+
     }
 }
