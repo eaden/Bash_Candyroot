44165cabb9f504be5d1e98ff028f1ce15a56d55a
Ingo2828 <dennis.schneeweiss@web.de>
2020-11-15 16:15:06 +0100

    finishCube

---
+++ Assets/Scripts/FinishScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishScript : MonoBehaviour
+{
+    Rigidbody finishCube;
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finishCube = GetComponent<Rigidbody>();
+    }
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "PlayerTag")
+        {
+            Debug.Log("Gewonnen");
+
+        }
+
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
